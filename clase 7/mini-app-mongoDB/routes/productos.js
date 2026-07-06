b6fac470151f907091ece3d422e685e95c73d173
pablormacia <pr.macia@gmail.com>
2026-07-06 16:02:23 -0300

    Clase 7-8. Paginación con skip y limit

--- clase 7/mini-app-mongoDB/routes/productos.js
+++ clase 7/mini-app-mongoDB/routes/productos.js
@@ -11,6 +11,10 @@ router.get("/", async (req, res) => {
 
   const filtro = {};
 
+  const page = Number(req.query.page) || 1;
+  const limit = 3;
+  const skip = (page - 1) * limit;
+
   // Categoría
   if (categoria) {
     filtro.categoria = categoria;
@@ -34,7 +38,11 @@ router.get("/", async (req, res) => {
     filtro.activo = true;
   }
 
-  const productos = await Producto.find(filtro);
+  const productos = await Producto.find(filtro).skip(skip).limit(limit);
+
+  const total = await Producto.countDocuments(filtro);
+
+  const totalPages = Math.ceil(total / limit);
 
   res.render("productos", {
     productos,
@@ -42,6 +50,16 @@ router.get("/", async (req, res) => {
     nombreActual: nombre || "",
     garantiaActual: garantia || "",
     activoActual: activo || "",
+
+    page,
+    totalPages,
+
+    hasPrev: page > 1,
+    hasNext: page < totalPages,
+
+    prevPage: page - 1,
+    nextPage: page + 1,
+
     error: req.query.error ? "Datos inválidos." : null,
   });
 });
@@ -51,20 +69,17 @@ router.get("/", async (req, res) => {
 // ==========================
 router.post("/crear", async (req, res) => {
   try {
-
     const datos = {
       ...req.body,
 
       precio: Number(req.body.precio),
       stock: Number(req.body.stock),
 
-      garantia: req.body.garantia
-        ? Number(req.body.garantia)
-        : undefined,
+      garantia: req.body.garantia ? Number(req.body.garantia) : undefined,
 
       tags: req.body.tags
-        ? req.body.tags.split(",").map(tag => tag.trim())
-        : []
+        ? req.body.tags.split(",").map((tag) => tag.trim())
+        : [],
     };
 
     const producto = new Producto(datos);
@@ -72,9 +87,7 @@ router.post("/crear", async (req, res) => {
     await producto.save();
 
     return res.redirect("/productos");
-
   } catch (error) {
-
     console.log(error.message);
 
     return res.redirect("/productos?error=1");
@@ -85,13 +98,11 @@ router.post("/crear", async (req, res) => {
 // ELIMINAR
 // ==========================
 router.post("/eliminar/:id", async (req, res) => {
-
   await Producto.deleteOne({
     _id: req.params.id,
   });
 
   res.redirect("/productos");
-
 });
 
 export default router;
